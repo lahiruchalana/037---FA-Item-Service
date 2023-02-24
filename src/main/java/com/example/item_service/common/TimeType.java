91547bef21cda7b71f921d5eea681533f7cf4231
Lahiru Chalana <lahiruchalana@incubatelabs.com>
2023-02-24 17:34:59 +0530

    create some relations and tables

---
+++ src/main/java/com/example/item_service/common/TimeType.java
@@ -0,0 +1,11 @@
+package com.example.item_service.common;
+
+public enum TimeType {
+    SECOND,
+    MINUTE,
+    HOUR,
+    DAY,
+    WEEK,
+    MONTH,
+    YEAR,
+}
--- src/main/java/com/example/item_service/entity/Allergens.java
+++
@@ -1,8 +0,0 @@
-package com.example.item_service.entity;
-
-public class Allergens {
-
-    /**
-     * Allergens: Any allergens that are present in the food, such as wheat, soy, nuts, or dairy, should be clearly labeled.
-     */
-}
