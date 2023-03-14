fa06a99deeb6091b1eb0ea76f0dd55f04ff35efd
Lahiru Chalana <lahiruchalana@incubatelabs.com>
2023-03-14 11:58:27 +0530

    change response in requests

--- src/main/java/com/baba/foods/food_service/controller/FoodController.java
+++ src/main/java/com/baba/foods/food_service/controller/FoodController.java
@@ -1,12 +1,14 @@
 package com.baba.foods.food_service.controller;
 
 import com.baba.foods.food_service.business.FoodService;
+import com.baba.foods.food_service.controller.controllconfig.ResponseHandler;
 import com.baba.foods.food_service.dto.AdditiveDTO;
 import com.baba.foods.food_service.dto.FoodDTO;
 import com.baba.foods.food_service.dto.response.ServiceResponseDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,66 +20,69 @@ public class FoodController {
     private final FoodService foodService;
 
     @PostMapping("/send")
-    public ServiceResponseDTO addNewFood(
+    public ResponseEntity<Object> addNewFood(
             @RequestBody FoodDTO foodDTO
     ) {
         log.info ("LOG :: FoodController addNewFood()");
         try {
             log.info ("LOG :: FoodController addNewFood() inside the try");
-            return foodService.addNewFood(foodDTO);
+            return ResponseHandler.responseBuilder(foodService.addNewFood(foodDTO));
         } catch (Exception e) {
             log.warn ("LOG :: FoodController addNewFood() inside the catch");
             log.error(e.getMessage());
-            ServiceResponseDTO serviceResponseDTO = new ServiceResponseDTO();
-            serviceResponseDTO.setError(e);
-            serviceResponseDTO.setMessage("Fail");
-            serviceResponseDTO.setCode("500");
-            serviceResponseDTO.setHttpStatus(HttpStatus.INTERNAL_SERVER_ERROR);
-            serviceResponseDTO.setDescription(e.getMessage());
-            return serviceResponseDTO;
+            ServiceResponseDTO serviceResponseDTO = ServiceResponseDTO.builder()
+                    .error(e)
+                    .message("Fail")
+                    .code("500")
+                    .httpStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .description(e.getMessage())
+                    .build();
+            return ResponseHandler.responseBuilder(serviceResponseDTO);
         }
     }
 
     @GetMapping("/get/page/{pageNumber}/size/{size}")
-    public ServiceResponseDTO getFoodDataWithPagination(
+    public ResponseEntity<Object> getFoodDataWithPagination(
             @PathVariable("pageNumber") Integer pageNumber,
             @PathVariable("size") Integer size
     ) {
         log.info ("LOG :: FoodController getFoodDataWithPagination()");
         try {
             log.info ("LOG :: FoodController getFoodDataWithPagination() inside the try");
-            return foodService.getFoodDataWithPagination(pageNumber, size);
+            return ResponseHandler.responseBuilder(foodService.getFoodDataWithPagination(pageNumber, size));
         } catch (Exception e) {
             log.warn ("LOG :: FoodController getFoodDataWithPagination() inside the catch");
             log.error(e.getMessage());
-            ServiceResponseDTO serviceResponseDTO = new ServiceResponseDTO();
-            serviceResponseDTO.setError(e);
-            serviceResponseDTO.setMessage("Fail");
-            serviceResponseDTO.setCode("500");
-            serviceResponseDTO.setHttpStatus(HttpStatus.INTERNAL_SERVER_ERROR);
-            serviceResponseDTO.setDescription(e.getMessage());
-            return serviceResponseDTO;
+            ServiceResponseDTO serviceResponseDTO = ServiceResponseDTO.builder()
+                    .error(e)
+                    .message("Fail")
+                    .code("500")
+                    .httpStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .description(e.getMessage())
+                    .build();
+            return ResponseHandler.responseBuilder(serviceResponseDTO);
         }
     }
 
     @PutMapping("id/{foodId}/add/additive")
-    public ServiceResponseDTO addAdditiveForFood(
+    public ResponseEntity<Object> addAdditiveForFood(
             @PathVariable("foodId") Long foodId,
             @RequestBody AdditiveDTO additiveDTO
     ) {
         log.info ("LOG :: FoodController addAdditiveForFood()");
         try {
             log.info ("LOG :: FoodController addAdditiveForFood() inside the try");
-            return foodService.addAdditiveForFood(foodId, additiveDTO);
+            return ResponseHandler.responseBuilder(foodService.addAdditiveForFood(foodId, additiveDTO));
         } catch (Exception e) {
             log.info ("LOG :: FoodController addAdditiveForFood() inside the catch");
-            ServiceResponseDTO serviceResponseDTO = new ServiceResponseDTO();
-            serviceResponseDTO.setError(e);
-            serviceResponseDTO.setMessage("Fail");
-            serviceResponseDTO.setCode("500");
-            serviceResponseDTO.setHttpStatus(HttpStatus.INTERNAL_SERVER_ERROR);
-            serviceResponseDTO.setDescription(e.getMessage());
-            return serviceResponseDTO;
+            ServiceResponseDTO serviceResponseDTO = ServiceResponseDTO.builder()
+                    .error(e)
+                    .message("Fail")
+                    .code("500")
+                    .httpStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .description(e.getMessage())
+                    .build();
+            return ResponseHandler.responseBuilder(serviceResponseDTO);
         }
     }
 
--- src/main/java/com/baba/foods/food_service/dto/response/ServiceResponseDTO.java
+++ src/main/java/com/baba/foods/food_service/dto/response/ServiceResponseDTO.java
@@ -1,20 +1,25 @@
 package com.baba.foods.food_service.dto.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class ServiceResponseDTO implements Serializable {
 
     Object data;
     Object error;
-    Object message;
+    String message;
     String code;
     HttpStatus httpStatus;
     String description;
